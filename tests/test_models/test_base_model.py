619ddba1813d042e21049f27313ee3ba23da51af
adentintime <arixbizz@gmail.com>
2018-06-12 23:43:30 +0000

    Update test_base_model.py docstring

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -9,7 +9,7 @@ import time
 class TestBaseModel(unittest.TestCase):
     """class TestBaseModel"""
     def test_base_model_class_membership_and_attributes(self):
-        """test that BaseModel is right class with correct attrs"""
+        """BaseModel is right class with correct attrs"""
         base = BaseModel()
         self.assertIsInstance(base, BaseModel)
         self.assertIsNotNone(base.id)
@@ -17,7 +17,7 @@ class TestBaseModel(unittest.TestCase):
         self.assertIsNotNone(base.updated_at)
 
     def test_base_model_attr_type(self):
-        """test that BaseModel attributes are correct type"""
+        """BaseModel attributes are correct type"""
         base = BaseModel()
         self.assertIsInstance(base.id, str)
         self.assertEqual(len(base.id), 36)
@@ -25,12 +25,12 @@ class TestBaseModel(unittest.TestCase):
         self.assertIsInstance(base.updated_at, datetime.datetime)
 
     def test_base_model_updated_at_matches_created_at_initialization(self):
-        """test that BaseModel updated_at is same as create_at"""
+        """BaseModel updated_at is same as create_at"""
         base = BaseModel()
         self.assertEqual(base.updated_at, base.created_at)
 
     def test_base_model_str_method(self):
-        """test that BaseModel str method creates accurate representation"""
+        """BaseModel str method creates accurate representation"""
         base = BaseModel()
         base_str = base.__str__()
         self.assertIsInstance(base_str, str)
@@ -39,16 +39,14 @@ class TestBaseModel(unittest.TestCase):
         self.assertDictEqual(eval(base_str[51:]), base.__dict__)
 
     def test_base_model_save_method(self):
-        """test that BaseModel save method alters update_at date"""
+        """BaseModel save method alters update_at date"""
         base = BaseModel()
-        before_date = base.updated_at
-        time.sleep(.1)
+        time.sleep(0.0001)
         base.save()
-        after_date = base.updated_at
-        self.assertNotEqual(before_date, after_date)
+        self.assertNotEqual(base.updated_at, base.created_at)
 
     def test_base_model_to_dict_method(self):
-        """test that BaseModel to_dict method creates accurate dictionary"""
+        """BaseModel to_dict method creates accurate dictionary"""
         base = BaseModel()
         base_dict = base.to_dict()
         self.assertIsInstance(base_dict, dict)
@@ -60,7 +58,7 @@ class TestBaseModel(unittest.TestCase):
         self.assertIsInstance(base.updated_at, datetime.datetime)
 
     def test_base_model_dict_to_instance_with_kwargs(self):
-        """test that BaseModel can instantiate new object with dictionary"""
+        """BaseModel can instantiate new object with dictionary"""
         base = BaseModel()
         base.name = "Betty"
         base.number = 972
@@ -71,7 +69,7 @@ class TestBaseModel(unittest.TestCase):
         self.assertDictEqual(new_base_dict, base_dict)
 
     def test_base_model_dict_to_instance_with_empty_kwargs(self):
-        """test that BaseModel can instantiate new object with empty dict"""
+        """BaseModel can instantiate new object with empty dict"""
         base_dict = {}
         new_base = BaseModel(**base_dict)
         new_base_dict = new_base.to_dict()
